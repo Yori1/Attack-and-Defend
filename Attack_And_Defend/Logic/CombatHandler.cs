02d055bc074c14aa8ba3fd7a2d39d68295eed33e
Yori Garcia Rosario <yorigr@outlook.com>
2018-10-22 15:33:23 +0200

    combatresults

---
+++ Attack_And_Defend/Logic/CombatHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Attack_And_Defend.Models;
+using Attack_And_Defend.Data;
+
+namespace Logic
+{
+    class CombatHandler
+    {
+        Party playerParty;
+        Party cpuParty;
+
+        Character playerCharacter;
+        Character cpuCharacter;
+
+        public CombatHandler(Party playerParty, Party cpuParty)
+        {
+            this.playerParty = playerParty;
+            this.cpuParty = cpuParty;
+            playerCharacter = playerParty.LeadCharacter;
+            cpuCharacter = cpuParty.LeadCharacter;
+        }
+
+        public void Attack()
+        {
+        }
+    }
+}
---
+++ Attack_And_Defend/Models/CombatResults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    class CombatResults
+    {
+        public int UserId { get; set; }
+        public int Won { get; set; }
+        public int Lost { get; set; }
+        public DateTime LastTimePlayed { get; set; }
+    }
+}
