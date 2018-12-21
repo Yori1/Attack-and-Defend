42119d50fb330cab697747a893ac8d544cc6820f
Yori Garcia Rosario <yorigr@outlook.com>
2018-12-21 12:22:01 +0100

    fixed data layer relations

---
+++ Attack_And_Defend/Attack_And_Defend/Controllers/AccountController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Web;
+using Microsoft.AspNetCore.Owin;
+using System.Security.Claims;
+
+using Attack_And_Defend.Logic;
+using Attack_And_Defend.Models;
+using Microsoft.AspNetCore.Routing;
+
+namespace Attack_And_Defend.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserHandler userHandler;
+
+        public HomeController(UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, Data.ApplicationDbContext context)
+        {
+             userHandler = new UserHandler(userManager, context, signInManager);
+        }
+
+        public IActionResult Index()
+        {
+            if (userHandler.GetSignedInUser(User) == null)
+                return View("Views/Home/NotLoggedIn.cshtml", new LoginViewModel(""));
+            else
+                return RedirectToAction("PartyOverview", "Party");
+        }
+
+        [HttpPost]
+        public IActionResult Register(string UsernameRegister, string PasswordRegister, string PasswordConfirm)
+        {
+
+            LoginViewModel vm;
+            if (PasswordRegister != PasswordConfirm)
+            {
+                vm = new LoginViewModel("Passwords are not identical.");
+                return View("Views/Home/NotLoggedIn.cshtml", vm);
+            }
+            var result = userHandler.TryCreateUser(UsernameRegister, PasswordRegister);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Login", "Home", new RouteValueDictionary(
+                    new { UsernameLogin = UsernameRegister, PasswordLogin = PasswordRegister }) );
+            }
+            else
+            {
+                vm = new LoginViewModel(result.Errors);
+                return View("Views/Home/NotLoggedIn.cshtml", vm);
+            }
+        }
+
+        public async Task<IActionResult> Login(string UsernameLogin, string PasswordLogin)
+        {
+            var result = userHandler.TryLogInUser(UsernameLogin, PasswordLogin);
+            if (result.Succeeded)
+                return RedirectToAction("PartyOverview", "Party");
+            else
+            {
+                LoginViewModel vm = new LoginViewModel("Failed login, make sure you registered using this password.");
+                return View("Views/Home/NotLoggedIn.cshtml", vm);
+            }
+        }
+
+        public IActionResult LogOut()
+        {
+            bool logoutSuccesful = userHandler.TryLogOut();
+            if (logoutSuccesful)
+                return View("Views/Home/NotLoggedIn.cshtml", new LoginViewModel(""));
+            return Index();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Attack_And_Defend/Models/GameState.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attack_And_Defend.Models
+{
+    public class GameState
+    {
+        public Party PlayerParty;
+        public Party CpuParty;
+
+        public List<Character> CharactersUsedByPlayer { get; private set; }
+        public string UsernameUser { get; private set; }
+        public string MessageLog { get; private set; }
+
+        public bool? PlayerWon { get; private set; }
+        public int TimesAttacked { get; private set; }
+        public int TimesSkillUsed { get; private set; }
+        public int CharactersDefeated { get; private set; }
+        public int TurnNumber { get; private set; }
+
+        Random random;
+
+        public void Attack()
+        {
+            Character target = CpuParty.GetRotatedInCharacter();
+            Character attacker = PlayerParty.GetRotatedInCharacter();
+
+            int initialHealth = target.RemainingHealth;
+            attacker.AttackTarget(target);
+            int damage = initialHealth - CpuParty.GetRotatedInCharacter().RemainingHealth;
+
+            MessageLog += attacker.Name + " attacks " + target.Name + " for " + damage + " damage." + Environment.NewLine;
+            TimesAttacked++;
+            TurnNumber++;
+
+            opponentTurn();
+        }
+
+        public void UseSkill()
+        {
+            Character userSkill = PlayerParty.GetRotatedInCharacter();
+            Character targetSkill = CpuParty.GetRotatedInCharacter();
+
+            userSkill.TryUseSkill(targetSkill);
+            TimesSkillUsed++;
+
+            opponentTurn();
+        }
+
+
+
+
+        void registerAttack(string nameAttacker, string nameTarget, int damage)
+        {
+
+        }
+
+        void registerDefeat(string nameAttacker, string nameDefeatedCharacter)
+        {
+            MessageLog += nameDefeatedCharacter + " was defeated." + Environment.NewLine;
+            CharactersDefeated++;
+        }
+
+        void RegisterWinOrLoss(bool won)
+        {
+            if (won)
+            {
+                MessageLog += UsernameUser + " won the battle." + Environment.NewLine;
+            }
+            else
+            {
+                MessageLog += UsernameUser + " lost the battle." + Environment.NewLine;
+            }
+
+            PlayerWon = won;
+        }
+
+
+
+        int getDamageFromPlayerAttack()
+        {
+
+        }
+
+        void opponentTurn()
+        {
+            ensureBothPartiesCanContinue();
+            if (PlayerWon == null)
+            {
+                letOpponentChooseMove();
+                ensureBothPartiesCanContinue();
+            }
+        }
+
+        void ensureBothPartiesCanContinue()
+        {
+            ensurePartyCanContinue(PlayerParty);
+            ensurePartyCanContinue(CpuParty);
+        }
+
+        void ensurePartyCanContinue(Party party)
+        {
+            if (party.GetRotatedInCharacter().Fainted)
+            {
+                bool partyHasCharacters = party.TryRotateCharacter();
+                if (!partyHasCharacters)
+                {
+                    bool playerWon = (party == CpuParty);
+                    RegisterWinOrLoss(playerWon);
+                }
+            }
+        }
+
+        void letOpponentChooseMove()
+        {
+            CharacterAction decision = getCpuDecision();
+            switch (decision)
+            {
+                case CharacterAction.Attack:
+                    CpuParty.GetRotatedInCharacter().AttackTarget(PlayerParty.GetRotatedInCharacter());
+                    break;
+
+                case CharacterAction.Skill:
+                    CpuParty.GetRotatedInCharacter().TryUseSkill(PlayerParty.GetRotatedInCharacter());
+                    break;
+            }
+        }
+
+        CharacterAction getCpuDecision()
+        {
+            random = new Random();
+            if (CpuParty.GetRotatedInCharacter().CanUseSkill)
+            {
+                if (random.Next(0, 2) == 1)
+                    return CharacterAction.Skill;
+            }
+            return CharacterAction.Attack;
+        }
+    }
+}
